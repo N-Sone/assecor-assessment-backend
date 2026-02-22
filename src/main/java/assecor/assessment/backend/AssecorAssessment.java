24809fa563af216f4203c2f749e975fe76c42ede
N-Sone <dentalsone@gmail.com>
2026-02-22 11:46:50 +0100

    Grund Struktur inkl. H2 Datenbank Anbindung

---
+++ src/main/java/assecor/assessment/backend/AssecorAssessment.java
@@ -0,0 +1,11 @@
+package assecor.assessment.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssecorAssessment {
+    public static void main(String[] args) {
+        SpringApplication.run(AssecorAssessment.class, args);
+    }
+}
---
+++ src/main/java/assecor/assessment/backend/model/Person.java
@@ -0,0 +1,24 @@
+package assecor.assessment.backend.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Person {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+    private String LastName;
+    private String zipcode;
+    private String city;
+    private String color;
+}
